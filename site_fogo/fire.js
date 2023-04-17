572f5b7c78c634f061496ac110004a76357eef47
JairoCatumbo <jairoreceadocatumbo@gmail.com>
2023-04-17 01:21:27 -0300

    finalizado o fogo
    fogo acabado

--- site_fogo/fire.js
+++ site_fogo/fire.js
@@ -1,13 +1,13 @@
 const PixelvetorFogo=[]
-const fogowidth=2
-const fogoHeigth=3
-
-
+const fogowidth=60
+const fogoHeigth=60
+const fireColorsPaletteRed = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}];
 function  iniciar(){
     criarEstruturaFogo()
-    renderizarFogo()
+    fonteDoFogo()
+    renderizarFogo() 
     
-
+    setInterval(PropagacaoFogo,-1000)
 } 
 
 function criarEstruturaFogo(){
@@ -19,24 +19,67 @@ function criarEstruturaFogo(){
 
 }
 
-function calcularPropagacaoFogo(){
+function PropagacaoFogo(){
+    for(let column=0;column<fogowidth;column++){
+        for(let row=0;row<fogoHeigth;row++){
+            const indicePixel= column+(fogowidth*row) 
+            
+            atualizarIntencidade(indicePixel)
+        }
+    }
+    renderizarFogo()
+}
+
+function atualizarIntencidade(correntPixelIndex){
+    const pixelDebaixo= correntPixelIndex +fogowidth
+    if(pixelDebaixo>=fogowidth*fogoHeigth){
+        return 
+    }
+    const desconto=Math.floor(Math.random()*2.6)
+    const intencidadeDopixelDebaixo=PixelvetorFogo[pixelDebaixo]
+    const novaIntencidade=intencidadeDopixelDebaixo-desconto>=0? intencidadeDopixelDebaixo-desconto:0
+    PixelvetorFogo[correntPixelIndex-desconto]=novaIntencidade
 
 }
+
+
 function renderizarFogo(){
+    debug=false
     let html='<table cellpadding=0 cellspacing=0>'
     for(let row=0;row<fogoHeigth;row++){
         html+='<tr>'
             for(let column=0;column<fogowidth;column++){
                 const indicePixel= column+(fogowidth*row)
-                html+='<td>'
-                html+= indicePixel
-                html+='</td>'
+                const intencidadeFogo=PixelvetorFogo[indicePixel]
+
+                if(debug===true){
+                    html+='<td>'
+                    html+=`<div class="pixel-index">${indicePixel}</div>`
+                    html+=intencidadeFogo
+                    html+='</td>'
+                }else{
+                    const color= fireColorsPaletteRed[intencidadeFogo]
+                    const colorString=`${color.r},${color.g},${color.b}`
+                    html+=`<td class="pixels" style="background-color:rgb(${colorString}) ">`
+                    html+=`</td>`
+                }
+
+              
             }
         html+='</tr>'
     }
     html+='</table>'
     document.querySelector('#fogoCanvas').innerHTML=html
 
+}
+
+function fonteDoFogo(){
+    for(let column=0;column<=fogowidth;column++){
+        const ultimopixelindex=fogowidth*fogoHeigth
+        const pixelIndex=(ultimopixelindex-fogowidth)+column
+
+        PixelvetorFogo[pixelIndex]=36
+    }
 }
 
 iniciar()
